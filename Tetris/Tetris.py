05d113b96d9e8ad3ad6d865daee93df3ebe78534
evanp28 <99385145+evanp28@users.noreply.github.com>
2022-04-01 19:02:05 -0500

    Co-authored-by: janowak3 <janowak3@users.noreply.github.com> Co-authored-by: qichen11 <stars-earth@users.noreply.github.com> Co-authored-by: Devon V <devonv2@users.noreply.github.com>

--- Tetris/Tetris.py
+++ Tetris/Tetris.py
@@ -1,14 +1,14 @@
 # from dataclasses import dataclass
 import queue
-from typing_extensions import Self
+# from typing_extensions import Self
 import random
 import pygame
 
-
-class Piece ():
+class Piece():
     def __init__(self, n_grid, n_color):
         self.n_grid = n_grid
         self.n_color = n_color
+
     def rot_ccw(self, piece):  # rotate couterclockwise
         rotpiece = [[0 for n in range(4)] for m in range(4)]
         for m in range(0, 4):
@@ -43,79 +43,77 @@ class Piece ():
             if Board.hit(board, piece, (x + i, y)) == True:
                 return (x+(i-1), y)
 
-
-def startGame():
-    queue = queue()
-    y = 7 * random()
-    i_piece = Piece([
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 0, 0, 0]]
+queue = []
+i_piece = Piece([
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 0, 0, 0]]
     , "blue")
-    t_piece = Piece([
-        [1, 0, 0, 0],
-        [1, 1, 0, 0],
-        [1, 0, 0, 0],
-        [0, 0, 0, 0]
+t_piece = Piece([
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 0]
     ], "yellow")
-    z_piece = Piece([
-        [1, 1, 0, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+z_piece = Piece([
+    [1, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
     ], "red")
-    s_piece = Piece([
-        [0, 1, 1, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0],
-        [0, 0, 0, 0]
+s_piece = Piece([
+    [0, 1, 1, 0],
+    [1, 1, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
     ], "orange")
-    j_piece = Piece([
-        [0, 1, 0, 0],
-        [0, 1, 0, 0], 
-        [1, 1, 0, 0],
-        [0, 0, 0, 0]
+j_piece = Piece([
+    [0, 1, 0, 0],
+    [0, 1, 0, 0], 
+    [1, 1, 0, 0],
+    [0, 0, 0, 0]
     ], "green")
-    o_piece = Piece([
-        [0, 0, 0, 0],
-        [0, 1, 1, 0],
-        [0, 1, 1, 0],
-        [0, 0, 0, 0]
+o_piece = Piece([
+    [0, 0, 0, 0],
+    [0, 1, 1, 0],
+    [0, 1, 1, 0],
+    [0, 0, 0, 0]
     ], "purple")
-    l_piece = Piece([
-        [1, 0, 0, 0],
-        [1, 0, 0, 0],
-        [1, 1, 0, 0],
-        [0, 0, 0, 0]
+l_piece = Piece([
+    [1, 0, 0, 0],
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 0, 0, 0]
     ], "pink")
+
+def gameStart():
+    y = random.randint(0, 6)
     all_pieces = [t_piece, i_piece, z_piece, s_piece, j_piece, o_piece, l_piece]
     queue.append(all_pieces[y])
 
 scores = 0
 
-# try to read highest record from saved file,jump over this step if fail
-
+    # try to read highest record from saved file,jump over this step if fail
 
 class Board:
-
     level = 1
 
     gameStart = False
     
     def __init__(self):
-        board = [[0 for n in range(14)] for m in range(22)]
+        board = [[0 for n in range(14)] for m in range(24)]
         for i in range(14):
-            for j in range(22):
+            for j in range(24):
                 if i < 2 or j > 19 or i > 11:
-                    board[j][i] = 1
+                    board[j][i] = 5
         print(*board, sep="\n")
 
     def add_to_board(self, board, piece, coords):
         x, y = coords
-        board_temp = [[0 for n in range(10)] for m in range(20)]
+        board_temp = [[0 for n in range(14)] for m in range(22)]
         for m in range(0, 20):
-            for n in range(0, 10):
+            for n in range(2, 12):
                 board_temp[m][n] = board[m][n]
         for m in range(x, x+4):
             for n in range(y, y+4):
@@ -123,38 +121,34 @@ class Board:
         return board_temp
 
     def hit(self, board, piece, coords):
-        board_temp = self.add_to_board(board, piece, coords)
-        for m in range(20):
-            for n in range(10):
-                if board_temp[m][n] > 1:
+        board_temp = self.add_to_board(board.copy(), piece, coords)
+        for m in range(22):
+            for n in range(14):
+                if board_temp[m][n] == 2 or board_temp[m][n] == 6:
                     return True
         return False
 
     def lineClear(self):  # check if a horizontal line is fullfilled and delete it, and add scores. --Qi Chen
         for i in self.board:
-            if i == [1 for n in range(14)]:
+            if i == [1 for n in range(16)]:
                 scores += 1  # replace '1' by the specific marks.
                 for m in range(1, self.board.index(i)):
                     self.board[m] = self.board[m-1]
 
 # save scores as a file if it's higher than the record
 
-x, y = 0, 3
-coords = (x, y)
-
-
-#print(*test, sep = "\n")
-
-
 count = 0
 fps = 25
+gameOn = True
+
 
 #Game Loop
-while True:
+while gameOn:
+    #adds pieces to queue 
     #Moving Pieces Down, can add level system very easy
     if (count % (fps // Board.level) == 0):
-        if Board.gameStart: 
-            Piece.push_down
+        if board.gameStart: 
+            piece.push_down
     count += 1
 
     #Inputs will be controlled using some sofware, I assume pygame
@@ -167,9 +161,22 @@ while True:
                 Piece.move_left
             if event.key == pygame.K_RIGHT:
                 Piece.move_right
+            if event.key == pygame.K_SPACE:
+                gameOn = False
+            if event.key == pygame.K_ESCAPE: 
+                piece_queue = 0 # piece that is pulled from queue, increases when piece is placed later
+                for i in range (200): #appends 200 pieces to queue when escape is hit(initialized)
+                    gameStart()
+    
+    Placed = False
+    #when a piece is added to board, make true
+    while Placed:
+        piece_queue += 1
+        Board.add_to_board(Board, queue[piece_queue], (0, 12))
 
-                
-    # generate initial pieces, use link list to generate 200 or so pieces initally -- Evan
+    
+
+
     # default coordinates for where pieces drop--row 22 to 23 -- Evan
     # do default coordinates change based on piece? what is the default position for each piece? -- Evan
 
